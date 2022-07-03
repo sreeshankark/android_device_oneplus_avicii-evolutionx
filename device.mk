acea03d47401cb44b2edbd5e54e44ca4c203fd90
AnierinB <anierin@evolution-x.org>
2022-07-03 13:53:00 +0900

    avicii: Configure & ship OPlusExtras
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -30,11 +30,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# Alert slider
-PRODUCT_PACKAGES += \
-    KeyHandler \
-    tri-state-key-calibrate
-
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
@@ -244,6 +239,7 @@ PRODUCT_PACKAGES += \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
+    init.oplus_extras.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
@@ -311,6 +307,11 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# OPlusExtras
+PRODUCT_PACKAGES += \
+    OPlusExtras \
+    tri-state-key-calibrate
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
@@ -329,6 +330,7 @@ PRODUCT_PACKAGES += \
     EvolutionSystemUIResTarget \
     FrameworksResTarget \
     FaceUnlockServiceResCommon \
+    OPlusExtrasResCommon \
     OPlusFrameworksResCommon \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
